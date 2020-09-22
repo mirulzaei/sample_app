fffcc8e91bdfce2ce8d1011dba8f18389676d074
mirulzaei <amirulazraei71@gmail.com>
2020-09-22 14:56:36 +0800

    Chapter_8

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,5 +2,4 @@ module ApplicationHelper
   def full_title page_title
     base_title = "Sample App"
     page_title.blank? ? base_title : "#{page_title} | #{base_title}"
-  end  
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,5 +3,6 @@ require('@rails/ujs').start()
 require('turbolinks').start()
 require('@rails/activestorage').start()
 require('channels')
-
+require('jquery')
+import 'bootstrap'
 
