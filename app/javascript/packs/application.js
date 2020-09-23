28b6dead067560700fc99172e62e0a72e9c71ab4
mirulzaei <amirulazraei71@gmail.com>
2020-09-23 09:36:43 +0800

    Chapter 9

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,6 @@ require('@rails/ujs').start()
 require('turbolinks').start()
 require('@rails/activestorage').start()
 require('channels')
-require('jquery')
-import 'bootstrap'
+require("jquery")
+import "bootstrap"
 
